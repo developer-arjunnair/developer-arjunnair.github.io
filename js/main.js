1ed09466b29b8292f9e1797aad84837e1e79413a
Arjun Nair <aku2anu@gmail.com>
2018-04-08 12:34:57 -0700

    uncommented the SW registeration

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 
   
   if ('serviceWorker' in navigator) {//Check if browser supports service worker
-    navigator.serviceWorker.register('js/sw/index.js');
-    // .then(function(reg) {
-    //   console.log('Registration worked!'+reg) ;
+    navigator.serviceWorker.register('js/sw/index.js')
+    .then(function(reg) {
+      console.log('Registration worked!'+reg) ;
   
-    //   if(!navigator.serviceWorker.controller){
-    //     return;
-    //   }
-    //   navigator.serviceWorker.addEventListener("controllerchange",function(){
-    //     location.reload();
-    //   });
+      if(!navigator.serviceWorker.controller){
+        return;
+      }
+      navigator.serviceWorker.addEventListener("controllerchange",function(){
+        location.reload();
+      });
   
-    //   let oInstalling = reg.installing;
-    //   console.log(sw.state);
+      let oInstalling = reg.installing;
+      console.log(sw.state);
 
-    //    /*
-    //   TODO - when waiting
-    //   */
+       /*
+      TODO - when waiting
+      */
         
 
-    //     /*
-    //   TODO - when state Change
-    //   */
+        /*
+      TODO - when state Change
+      */
   
         
-    //   /*
-    //   TODO - UpdateFound
-    //   */
+      /*
+      TODO - UpdateFound
+      */
       
     
-    // }).catch(function() {
-    //   console.log('Registration failed!');
-    // });
+    }).catch(function() {
+      console.log('Registration failed!');
+    });
       }
 
 
