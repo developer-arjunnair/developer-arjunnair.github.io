7cf4b2dc883b8e137dbdea7a99c61b0b0536d166
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2018-04-08 13:15:19 -0700

    sync from main repo

--- js/sw/index.js
+++ js/sw/index.js
@@ -2,39 +2,39 @@ const CacheName = "GWGinNutShell-V1";
 
 self.addEventListener('install', function(event) {
     console.log("within install");
-    event.waitUntil(
-     /*
-     TODO after installing
-     */
-    );
+    // event.waitUntil(
+    //  /*
+    //  TODO after installing
+    //  */
+    // );
   });
 
 
   self.addEventListener("activate",function(oEvent){
     console.log("within activate");
-	oEvent.waitUntil(
-		/*
-     TODO after activate
-     */
-	);
+	// oEvent.waitUntil(
+	// 	/*
+    //  TODO after activate
+    //  */
+	// );
     });
 
 
 self.addEventListener("fetch",function(oEvent){
     console.log("within fetch");
-    oEvent.waitUntil(
-        /*
-        TODO after fetch
-        */
-    );
+    // oEvent.waitUntil(
+    //     /*
+    //     TODO after fetch
+    //     */
+    // );
     });
 
 self.addEventListener("message",function(oEvent){
     console.log("within message");
-    oEvent.waitUntil(
-        /*
-        TODO after message
-        */
-    );
+    // oEvent.waitUntil(
+    //     /*
+    //     TODO after message
+    //     */
+    // );
     });
 
