5d4aba04057394e8a0189c33abf6c8c79be19df3
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2018-04-08 11:24:39 -0700

    intial commit
    Commit with
    1. index.html
    2. main.js and
    3. sw.js

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+  
+  if ('serviceWorker' in navigator) {//Check if browser supports service worker
+    navigator.serviceWorker.register('js/sw/index.js');
+    // .then(function(reg) {
+    //   console.log('Registration worked!'+reg) ;
+  
+    //   if(!navigator.serviceWorker.controller){
+    //     return;
+    //   }
+    //   navigator.serviceWorker.addEventListener("controllerchange",function(){
+    //     location.reload();
+    //   });
+  
+    //   let oInstalling = reg.installing;
+    //   console.log(sw.state);
+
+    //    /*
+    //   TODO - when waiting
+    //   */
+        
+
+    //     /*
+    //   TODO - when state Change
+    //   */
+  
+        
+    //   /*
+    //   TODO - UpdateFound
+    //   */
+      
+    
+    // }).catch(function() {
+    //   console.log('Registration failed!');
+    // });
+      }
+
+
+
+
---
+++ js/sw/index.js
@@ -0,0 +1,40 @@
+const CacheName = "GWGinNutShell-V1";
+
+self.addEventListener('install', function(event) {
+    console.log("within install");
+    event.waitUntil(
+     /*
+     TODO after installing
+     */
+    );
+  });
+
+
+  self.addEventListener("activate",function(oEvent){
+    console.log("within activate");
+	oEvent.waitUntil(
+		/*
+     TODO after activate
+     */
+	);
+    });
+
+
+self.addEventListener("fetch",function(oEvent){
+    console.log("within fetch");
+    oEvent.waitUntil(
+        /*
+        TODO after fetch
+        */
+    );
+    });
+
+self.addEventListener("message",function(oEvent){
+    console.log("within message");
+    oEvent.waitUntil(
+        /*
+        TODO after message
+        */
+    );
+    });
+
