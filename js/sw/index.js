3c8778d42068acb1e2ab8d4f92bb549233964bb0
Arjun Nair <aku2anu@gmail.com>
2018-04-08 12:37:38 -0700

    updated to resolve the waitUntil

--- js/sw/index.js
+++ js/sw/index.js
@@ -2,39 +2,40 @@ const CacheName = "GWGinNutShell-V1";
 
 self.addEventListener('install', function(event) {
     console.log("within install");
-    event.waitUntil(
+    event.waitUntil(function(){
      /*
      TODO after installing
      */
+    }
     );
   });
 
 
   self.addEventListener("activate",function(oEvent){
     console.log("within activate");
-	oEvent.waitUntil(
-		/*
-     TODO after activate
+	oEvent.waitUntil(function(){
+     /*
+     TODO after installing
      */
-	);
+    });
     });
 
 
 self.addEventListener("fetch",function(oEvent){
     console.log("within fetch");
-    oEvent.waitUntil(
-        /*
-        TODO after fetch
-        */
-    );
+    oEvent.waitUntil(function(){
+     /*
+     TODO after installing
+     */
+    });
     });
 
 self.addEventListener("message",function(oEvent){
     console.log("within message");
-    oEvent.waitUntil(
-        /*
-        TODO after message
-        */
-    );
+    oEvent.waitUntil(function(){
+     /*
+     TODO after installing
+     */
+    });
     });
 
