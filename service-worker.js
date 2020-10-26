79418645dd4e4c161e2a0a22e90fb8b80cbc8862
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2020-10-26 01:02:51 -0400

    refined version

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ec83cad4f7f2a7c734e1e4dc744b9a8.js"
+  "/precache-manifest.13dcd37ae01651e9ec22ca853360039b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
